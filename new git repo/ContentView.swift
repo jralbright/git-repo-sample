4cc5d053cab12e939c3d5d7289a686f0ac7e9fc5
Joseph Albright <jraprogramming@gmail.com>
2023-11-01 10:48:34 -0500

    First Commit

--- new git repo/ContentView.swift
+++ new git repo/ContentView.swift
@@ -2,18 +2,20 @@
 //  ContentView.swift
 //  new git repo
 //
-//  Created by AC Student on 11/1/23.
+//  Created by Joseph Albright on 11/1/23.
 //
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "1.square.fill")
+                .resizable()
+                .scaledToFit()                .foregroundStyle(.blue)
+            
+            Text("Version 1.0")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
         }
         .padding()
     }
