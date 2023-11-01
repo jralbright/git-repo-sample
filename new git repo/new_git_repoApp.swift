41dbefa6f49ed25fc1928fd045c18ec041347cb6
Joseph Albright <jraprogramming@gmail.com>
2023-11-01 10:43:26 -0500

    Initial Commit

---
+++ new git repo/new_git_repoApp.swift
@@ -0,0 +1,17 @@
+//
+//  new_git_repoApp.swift
+//  new git repo
+//
+//  Created by AC Student on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct new_git_repoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
